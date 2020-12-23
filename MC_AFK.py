10f546692586499ecbd12c2e49a748c6de4b4238
*Frezo* <wilczewski.dominik@gmail.com>
2020-12-23 16:58:39 +0100

    Create MC_AFK.py

---
+++ MC_AFK.py
@@ -0,0 +1,14 @@
+import time
+import multiprocessing
+import keyboard
+
+
+def mine():
+    kilof = int(input('Pick a pickaxe type: \n wooden -> 1 \n stone -> 2 \n golden -> 3 \n iron ->  4 \n diamond -> 5 \n netherite -> 6'))
+
+
+### running processes
+
+if __name__ == '__main__':
+    p1.start()
+    p2.start()
